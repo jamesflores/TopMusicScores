c83c953d9327bfab4eeb0f05dd61095858582b76
James Flores <james@jamesflores.net>
2024-05-09 23:22:02 +1000

    add Elasticsearch capability, but not yet implemented in view (using Django search with Q objects for now)

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('product/<str:title_slug>/', views.product_view, name='product'),
     path('about/', views.about, name='about'),
+    path('search/', views.search, name='search')
 ]
--- smp/settings.py
+++ smp/settings.py
@@ -137,3 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Elasticsearch settings
+
+ELASTICSEARCH_URL = os.getenv('ELASTICSEARCH_URL')
+ELASTICSEARCH_INDEX = os.getenv('ELASTICSEARCH_INDEX', 'sheet_music')
